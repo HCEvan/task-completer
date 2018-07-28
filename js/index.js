e5088ccdd5f7c3729eba9a77fb4213b61a1a6816
Calvin <calvintiley@hotmail.co.uk>
2018-07-28 13:06:15 +0100

    Console Logs added for readibility.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+// Creating Task on button 'add'.
 const submitBtn = document.querySelector('#submitBtn');
 const taskTable = document.querySelector('#taskTable');
 
@@ -6,31 +9,38 @@ submitBtn.addEventListener('click', function() {
 	if (userInput == "") {
 		alert("Please enter a task.");
 	} else {
-		newRow = taskTable.insertRow(); //create table row
-		console.log("Row Created.");
-	
-		taskCell = newRow.insertCell();
-		taskCell.textContent = userInput;
-		taskCell.classList.add('taskFlex');	//create task cell in new row and set its content = to userInput
 
-		dateCell = newRow.insertCell();
-		date = new Date();
+		const newRow = taskTable.insertRow(); //create table row
+		console.log("Row Created.");
+
+		const taskCell = newRow.insertCell();
+		taskCell.textContent = userInput;
+		taskCell.classList.add('taskFlex');	//create task cell in new row and set its content equal to userInput
+		console.log("Task Column Created.");
+
+		const dateCell = newRow.insertCell();
+		const date = new Date();
 		dateCell.textContent = date;
 		dateCell.classList.add('dateFlex');
+		console.log("Date Column Created.");
 
-		removeCell = newRow.insertCell();
-		removeBtn = document.createElement('BUTTON');
-		btnText = document.createTextNode('Remove Task');
+		const removeCell = newRow.insertCell();
+		const removeBtn = document.createElement('BUTTON');
+		const btnText = document.createTextNode('Remove Task');
 		removeBtn.classList.add('btn');
 		removeBtn.classList.add('btn-danger');
-		
+		console.log("Remove Column Created.");
+
 		removeBtn.appendChild(btnText);
 		removeCell.appendChild(removeBtn);
 		removeCell.classList.add('removeFlex');
-		
+
 		removeBtn.addEventListener('click', function() {
 			this.parentNode.parentNode.remove();
 			console.log("Row Deleted.");
 		});
+
+		console.log("Task Creation Complete.");
+
 	}
 });
