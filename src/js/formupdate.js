063dc55ee7ff07287c5c10f26d17d209a382ac35
Calvin <calvintiley@hotmail.co.uk>
2018-07-28 16:16:18 +0100

    Removed bootstrap; Moved code for readability; Created dynamic table creation/removal.

--- src/js/formupdate.js
+++ src/js/formupdate.js
@@ -7,36 +7,59 @@ form.addEventListener('submit', (e) => {
     const createTask = () => {
         if (input.length === 0) {
             alert("Please insert a task.");
-        } else if (!document.querySelector('#table')) {
-            const table = document.createElement('table');
-            const tableRow = document.createElement('tr');
-            
-            const taskHead = document.createElement('th');
-            const taskNode = document.createTextNode("Task");
-
-            const dateHead = document.createElement('th');
-            const dateNode = document.createTextNode("Date");
-
-            const removeHead = document.createElement('th');
-            const removeNode = document.createTextNode("Remove");
-            
-            table.appendChild(tableRow);
+        } else  { 
+            if (!document.querySelector('#table')) {
+                const table = document.createElement('table');
+                document.querySelector('#taskTable').appendChild(table);
+                table.id = 'table';
+                console.log('Table created.');
+                
+                const tableRow = document.createElement('tr');
+                tableRow.id = 'tableRow';
+                table.appendChild(tableRow);
+                
+                const taskHead = document.createElement('th');
+                tableRow.appendChild(taskHead);
+                const taskNode = document.createTextNode("Task");
+                taskHead.appendChild(taskNode);
     
-            tableRow.appendChild(taskHead);
-            taskHead.appendChild(taskNode);
+                const dateHead = document.createElement('th');
+                tableRow.appendChild(dateHead);
+                const dateNode = document.createTextNode("Date");
+                dateHead.appendChild(dateNode);
+    
+                const removeHead = document.createElement('th');
+                tableRow.appendChild(removeHead);
+            }
 
-            tableRow.appendChild(dateHead);
-            dateHead.appendChild(dateNode);
+            const taskRow = document.createElement('tr');
+            table.appendChild(taskRow);
+            taskRow.class = "taskRow";
+    
+            const taskText = document.createElement('td');
+            taskRow.appendChild(taskText);
+            const taskNode = document.createTextNode(input);
+            taskText.appendChild(taskNode);
+    
+            const dateText = document.createElement('td');
+            taskRow.appendChild(dateText);
+            const dateNode = document.createTextNode(new Date());
+            dateText.appendChild(dateNode);
+    
+            const removeText = document.createElement('td');
+            taskRow.appendChild(removeText);
+            const removeBtn = document.createElement('button');
+            removeText.appendChild(removeBtn).appendChild(document.createTextNode('Delete'));
+            removeBtn.addEventListener('click', () => {
+                if (table.children.length > 2) {
+                    taskRow.parentNode.removeChild(taskRow);
+                } else {
+                    table.parentNode.removeChild(table);
+                }
+            });
 
-            tableRow.appendChild(removeHead);
-            removeHead.appendChild(removeNode);
-            
 
-            document.querySelector('#taskTable').appendChild(table);
-        } else {
-
-        }
+        };
     };
-    console.log('Table created.');
     createTask();
-})
+});
